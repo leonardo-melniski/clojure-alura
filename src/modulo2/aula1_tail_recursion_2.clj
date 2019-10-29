0f20809ec95b938445dcedd25974b5eaedc946d4
Leonardo <leonardo.melniski@beetools.com.br>
2019-10-29 08:25:49 -0300

    [aula2] clojure optimize memory of recursion with command 'recur' on tail position

--- src/modulo2/aula1_tail_recursion_2.clj
+++ src/modulo2/aula1_tail_recursion_2.clj
@@ -11,3 +11,16 @@
 
 ; example
 ; (my-map println (range 100000))
+
+
+; optimize stack memory -> clojure use 'recur'
+; recur only in tail of function
+(defn my-map
+  [func inner-sequence]
+  (let [frst (first inner-sequence)]
+    (if (not (nil? frst))
+      (do
+        (func frst)
+        (recur func (rest inner-sequence))))))
+
+(my-map println (range 10000))
